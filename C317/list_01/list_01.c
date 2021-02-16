7fc64fe9e1a52dc7b56f185a5f2024148bf146f4
temikfart <fartygin.a@phystech.edu>
2021-02-16 11:52:07 +0300

    list_create as function

---
+++ C317/list_01/list_01.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef AAA
+
+typedef int Data;
+struct Node {
+    Data val;
+    struct Node * next;
+};
+typedef struct Node * List;
+
+List list_create ();
+
+int main()
+{
+    /* struct Node * list = list_create();
+    
+    printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
+    
+    Data x = 15;
+    list = list_push(list, x);
+    printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
+    
+    printf("list get: %d\n", list_get(list));
+    
+    list = list_push(list, 14);
+    list = list_push(list, 13);
+    list = list_push(list, 12);
+    
+    list_print(list); // 12 13 14 15
+    printf("Size: %d\n", list_size(list));
+    
+    Data y = 0;
+    list = list_clear(list);
+    list = list_push(list, x);
+    
+    list = list_pop(list, &y);
+    printf("pop: %d\n", y);
+    printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO"); */
+	
+	List list = list_create();
+    
+    //free(list);
+    return 0;
+}
+
+#endif
+
+List list_create ()
+{
+    return NULL;
+}
