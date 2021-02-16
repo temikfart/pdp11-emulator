35b0b14e7c2aa0abd6d70188f81fd5a3781916b6
temikfart <fartygin.a@phystech.edu>
2021-02-16 11:18:44 +0300

    conditional compilation

--- C317/list_02/list_02.c
+++ C317/list_02/list_02.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef AAA
+
 typedef int Data;
 struct Node {
     Data val;
@@ -15,6 +17,39 @@ void list_print (struct Node * list);
 int list_size(struct Node * list);
 int list_is_empty(struct Node * list);
 struct Node * list_clear(struct Node * list);
+
+int main()
+{
+	struct Node * list = list_create();
+	
+	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
+	
+	Data x = 15;
+	list = list_push(list, x);
+	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
+	
+	printf("list get: %d\n", list_get(list));
+	
+	list = list_push(list, 14);
+	list = list_push(list, 13);
+	list = list_push(list, 12);
+	
+	list_print(list); // 12 13 14 15
+	printf("Size: %d\n", list_size(list));
+	
+	Data y = 0;
+	list = list_clear(list);
+	list = list_push(list, x);
+	
+	list = list_pop(list, &y);
+	printf("pop: %d\n", y);
+	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
+	
+	free(list);
+	return 0;
+}
+
+#endif
 
 struct Node * list_create ()
 {
@@ -69,35 +104,4 @@ struct Node * list_clear(struct Node * list)
 	while(p != NULL)
 		p = list_pop(p, &x);
 	return p;
-}
-
-int main()
-{
-	struct Node * list = list_create();
-	
-	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
-	
-	Data x = 15;
-	list = list_push(list, x);
-	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
-	
-	printf("list get: %d\n", list_get(list));
-	
-	list = list_push(list, 14);
-	list = list_push(list, 13);
-	list = list_push(list, 12);
-	
-	list_print(list); // 12 13 14 15
-	printf("Size: %d\n", list_size(list));
-	
-	Data y = 0;
-	list = list_clear(list);
-	list = list_push(list, x);
-	
-	list = list_pop(list, &y);
-	printf("pop: %d\n", y);
-	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
-	
-	free(list);
-	return 0;
 }
