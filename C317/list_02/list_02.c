8293f338a558d7a4806d48d34a4d42ccdab031e8
temikfart <fartygin.a@phystech.edu>
2021-02-16 10:13:20 +0300

    list_push as function + test

--- C317/list_02/list_02.c
+++ C317/list_02/list_02.c
@@ -8,11 +8,20 @@ struct Node {
 };
 
 struct Node * list_create ();
+struct Node * list_push (struct Node * list, Data x);
 int list_is_empty(struct Node * list);
 
 struct Node * list_create ()
 {
 	return NULL;
+}
+struct Node * list_push (struct Node * list, Data x)
+{
+	struct Node * list2 = NULL;
+	list2 = malloc(sizeof(struct Node));
+	list2->val = x;
+	list2->next = list;
+	return list2;
 }
 int list_is_empty(struct Node * list)
 {
@@ -25,5 +34,10 @@ int main()
 	
 	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
 	
+	Data x = 15;
+	list = list_push(list, x);
+	printf("Empty: %s\n", list_is_empty(list) ? "YES" : "NO");
+	
+	free(list);
 	return 0;
 }
