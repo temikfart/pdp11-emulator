ce2544c12ffc5e5185ee98ce967fbcacf487e626
temikfart <fartygin.a@phystech.edu>
2021-02-16 11:16:15 +0300

    list_size, list_clear as finctions + tests. Check in list_print on empty list.

--- C317/list_02/list_02.c
+++ C317/list_02/list_02.c
@@ -12,7 +12,9 @@ struct Node * list_push (struct Node * list, Data x);
 struct Node * list_pop (struct Node * plist, Data * px);
 Data list_get(struct Node * list);
 void list_print (struct Node * list);
+int list_size(struct Node * list);
 int list_is_empty(struct Node * list);
+struct Node * list_clear(struct Node * list);
 
 struct Node * list_create ()
 {
@@ -39,15 +41,34 @@ Data list_get(struct Node * list)
 }
 void list_print (struct Node * list)
 {
+	if(list_is_empty(list))
+		printf("List is empty.\n");
 	for(struct Node * p = list; p != NULL; p = p->next)
 	{
 		printf("%d ", p->val);
 	}
 	printf("\n");
 }
+int list_size(struct Node * list)
+{
+	int n = 0;
+	for(struct Node * p = list; p != NULL; p = p->next)
+	{
+		n++;
+	}
+	return n;
+}
 int list_is_empty(struct Node * list)
 {
 	return list == NULL;
+}
+struct Node * list_clear(struct Node * list)
+{
+	Data x;
+	struct Node * p = list;
+	while(p != NULL)
+		p = list_pop(p, &x);
+	return p;
 }
 
 int main()
@@ -67,11 +88,11 @@ int main()
 	list = list_push(list, 12);
 	
 	list_print(list); // 12 13 14 15
+	printf("Size: %d\n", list_size(list));
 	
 	Data y = 0;
-	list = list_pop(list, &y);
-	list = list_pop(list, &y);
-	list = list_pop(list, &y);
+	list = list_clear(list);
+	list = list_push(list, x);
 	
 	list = list_pop(list, &y);
 	printf("pop: %d\n", y);
