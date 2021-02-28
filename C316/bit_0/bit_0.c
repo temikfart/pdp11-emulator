f0f72b88456e2dfc6bc1af3e45090af38957d365
temikfart <fartygin.a@phystech.edu>
2021-02-28 12:05:11 +0300

    printBit function

--- C316/bit_0/bit_0.c
+++ C316/bit_0/bit_0.c
@@ -1,1 +1,30 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printBit(unsigned char a)
+{
+// значения битов записываются в строку
+        char * tmp = calloc(10, sizeof(char));
+// 8 знаков в строке устанавливаются равными символу '0'
+        memset(tmp, '0', sizeof(char) * 8);
+// Заполняем со старших адресов
+        for (int i = 7 ; i > -1; i--)
+        {
+                tmp[i] = a % 2 + '0'; 
+                a /= 2;
+        }
+
+    printf("%s\n", tmp);
+    free(tmp);
+}
+
+int main()
+{
+    unsigned char x, y, res;
+    scanf("%hhu %hhu", x, y);
+    
+    // XOR = (x | ~y) & (~x | y)
+    
+    return 0;
+}
