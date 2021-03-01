6379bec1c55e0c9157dee5ff0c79cc99c4da0acb
temikfart <fartygin.a@phystech.edu>
2021-03-01 18:57:43 +0300

    print test + ma or fe detecting with print

--- C316/bit_1/bit_1.c
+++ C316/bit_1/bit_1.c
@@ -3,10 +3,16 @@
 
 int main()
 {
-    unsigned int n, a;
+    unsigned int n;
     
     scanf("%x", &n);
     printf("Num: %x\n", n);
+    
+    // ma = ???? ???1; fe = ???? ???0
+    if(n & 1)
+        printf("ma\n");
+    else
+        printf("fe\n");
     
     return 0;
 }
