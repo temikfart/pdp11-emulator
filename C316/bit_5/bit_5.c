a1fb526597401c471efe26086615e030d8622969
temikfart <fartygin.a@phystech.edu>
2021-03-02 11:36:16 +0300

    print tokens

--- C316/bit_5/bit_5.c
+++ C316/bit_5/bit_5.c
@@ -15,5 +15,14 @@ int main()
     
     printf("line: %sLen: %ld\n", descr, lend);
     
+    const char * delim = " \n\t\r";    // алфавит разделителей
+    
+    for (char * p = strtok(descr, delim);   // начинаем разбор, передаем строку s 
+            p != NULL;                  // пока не нашли новый разделитель
+            p = strtok(NULL, delim))  // в следующий раз вызываем от NULL
+    {
+        printf("%s\n", p);
+    }
+    
     return 0;
 }
