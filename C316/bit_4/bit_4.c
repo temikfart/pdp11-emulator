f176ae5df4e16596462ac0bcb879241cd14f9320
temikfart <fartygin.a@phystech.edu>
2021-03-02 08:29:37 +0300

    hair det as function

--- C316/bit_4/bit_4.c
+++ C316/bit_4/bit_4.c
@@ -11,11 +11,18 @@ void det_sex(unsigned int n)
 }
 void det_brain(unsigned int n)
 {
-    //if(n & 4)
     if(n >> 1)
         printf("cl\n");
     else
         printf("du\n");
+}
+void det_hair(unsigned int n)
+{
+    //if(n & 4)
+    if(n >> 2)
+        printf("ha\n");
+    else
+        printf("nh\n");
 }
 
 int main()
@@ -27,6 +34,7 @@ int main()
     
     det_sex(n);
     det_brain(n);
+    det_hair(n);
     
     return 0;
 }
