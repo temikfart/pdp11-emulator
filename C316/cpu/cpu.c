3eb0ab18a1f02960b7a86d395168bd5aef6fbf6b
temikfart <fartygin.a@phystech.edu>
2021-03-21 10:09:17 +0300

    new idea with token; HLT and MOV were realised; print_reg as function

--- C316/cpu/cpu.c
+++ C316/cpu/cpu.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define A 5
+#define B 6
+#define C 7
+#define D 8
+
+void print_reg();
 char ADD (char x, char y);
 char SUB (char x, char y);
 char MOV (char x, char y);
@@ -9,36 +15,63 @@ void IR();
 
 int main()
 {
-    char A, B, C, D;
-    A = B = C = D = 0;
-    
-    printf("%d %d %d %d\n", A, B, C, D);
-    
-    
-    char * descr = NULL;
-    size_t lend;
-    
-    if(getline(&descr, &lend, stdin) == -1)
+    char reg[4];
+    for(int k = 0; k<4; k++)
     {
-        printf("Nothing\n");
-        return 0;
+        reg[k] = 0;
     }
+    print_reg(reg);
     
-    
-    const char * delim = " \n\t\r";
-    for (char * p = strtok(descr, delim);
-            p != NULL;
-            p = strtok(NULL, delim))
+    while(1)
     {
-        if(strcmp(p, "0") == 0)
+        char * descr = NULL;
+        size_t lend;
+        
+        if(getline(&descr, &lend, stdin) == -1)
+        {
+            printf("Nothing\n");
+            return 0;
+        }
+    
+        char *temp[3];
+        int i = 0;
+    
+        const char * delim = " \n\t\r";
+        for (char * p = strtok(descr, delim);
+                p != NULL;
+                p = strtok(NULL, delim))
+        {
+            temp[i] = p;
+            i++;
+        }
+        if(strcmp(temp[0], "0") == 0)
         {
             printf("HLT\n");
+            print_reg(reg);
+            free(descr);
             break;
         }
-        //else if(strcmp(p, "1")
+        else if(strcmp(temp[0], "3") == 0)
+        {
+            int x = *temp[1] - '0' - A;
+            char y = *temp[2] - '0';
+            reg[x] = MOV(reg[x], y);
+            printf("MOV: x=%d; y=%d;\n", reg[x], y);
+            print_reg(reg);
+        }
     }
-
-    free(descr);
     
     return 0;
+}
+
+void print_reg(char *reg)
+{
+    for(int q = 0; q < 4; q++)
+        printf("%d ", reg[q]);
+    printf("\n");
+}
+char MOV (char x, char y)
+{
+    x += y;
+    return x;
 }
