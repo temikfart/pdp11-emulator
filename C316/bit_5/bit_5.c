1d8ceb9e0e02c8db54ec1e06fc44ac41f81b8d7b
temikfart <fartygin.a@phystech.edu>
2021-03-22 14:41:21 +0300

    some marks

--- C316/bit_5/bit_5.c
+++ C316/bit_5/bit_5.c
@@ -19,29 +19,44 @@ int main()
     
     //характеристики
     //будем хранить характеристики в массиве строк
-    char *sex[] = {"ma", "fe"};
-    char *brain[] = {"cl", "du"};
-    char *hat[] = {"ha", "nh"};
-    char *bottom[] = {"tr", "sk"};
-    char *hair_col[] = {"bn", "rd", "bw", "bk"};
-    char *eyes_col[] = {"bu", "ge", "gy", "da"};
-    printf("%s %s %s %s\n", sex[0], hat[2], eyes_col[3], bottom[0]);
+    char *sex[] = {"fe", "ma"};
+    //char *brain[] = {"cl", "du"};
+    //char *hat[] = {"ha", "nh"};
+    //char *bottom[] = {"tr", "sk"};
+    //char *hair_col[] = {"bn", "rd", "bw", "bk"};
+    //char *eyes_col[] = {"bu", "ge", "gy", "da"};
+    //printf("%s %s %s %s\n", sex[0], hat[2], eyes_col[3], bottom[0]);
     
     
     //Будем заполнять описание в массиве
-    char * temp = NULL;
+    char *temp[6];
+    for(int i = 0; i<6; i++)
+    {
+        temp[i] = 0;
+    }
     printf("Size of temp: %ld\n\n", sizeof(temp));
+    
+    unsigned int n = 0;
     
     for (char * p = strtok(descr, delim);   // начинаем разбор, передаем строку s 
             p != NULL;                  // пока не нашли новый разделитель
             p = strtok(NULL, delim))  // в следующий раз вызываем от NULL
     {
-        printf("%s: ", p);
+        //printf("%s: ", p);
         
         //Проверка на характеристику: ma/fe
         //if(strcmp(p, "ma") == 0 || strcmp(p, "fe") == 0)
-        if(1) //нужно в условии проверить на ma/fe
+        if(temp[0] == 0 && (strcmp(p, sex[0]) == 0 || strcmp(p, sex[1]) == 0) ) //проверка на отсутствие описания по этому параметру
         {
+            for(size_t i = 0; i < sizeof(sex)/sizeof(sex[0]); i++)
+            {
+                if(strcmp(p, sex[i]) == 0)
+                {
+                    strcat(temp[0], sex[i]);
+                    n += i;
+                    printf("%s\n%hhd\n", temp[0], n);
+                }
+            }
             printf("ma_fe\n");
         }
         else
@@ -52,7 +67,6 @@ int main()
     }
     
     free(descr);
-    free(temp);
     
     return 0;
 }
