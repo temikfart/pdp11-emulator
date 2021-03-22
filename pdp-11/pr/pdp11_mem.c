74b1e2c75f611ee5d9c0405e57c057f175763c28
temikfart <fartygin.a@phystech.edu>
2021-03-22 16:25:20 +0300

    word write + test_mem upd; w-w\w-r, w-w/2b-r test

--- pdp-11/pr/pdp11_mem.c
+++ pdp-11/pr/pdp11_mem.c
@@ -21,7 +21,7 @@ void test_mem()
     //Пишем байт, читаем байт
     b_write(2, b0);
     byte bres = b_read(2);
-    //printf("%02hhx = %02hhx\n", b0, bres);
+    printf("b-w/b-r \t %02hhx = %02hhx\n", b0, bres);
     assert(b0 == bres);
     
     //Пишем 2 байта, читаем слово
@@ -32,8 +32,26 @@ void test_mem()
     b_write(a, b0);
     b_write(a + 1, b1);
     word wres = w_read(a);
-    //printf("ww/br \t %04hx = %02hhx%02hhx\n", wres, b1, b0);
+    printf("2b-w/w-r \t %04hx = %02hhx%02hhx\n", wres, b1, b0);
     assert(w == wres);
+    
+    //Пишем слово, читаем слово
+    Adress a2 = 6;
+    word w2 = 0xfd0e;
+    w_write(a2, w2);
+    word wres2 = w_read(a2);
+    printf("w-w/w-r \t %04hx = %04hx\n", wres2, w2);
+    assert(w2 == wres2);
+    
+    //Пишем слово, читаем 2 байта
+    Adress a3 = 8;
+    word w3 = 0xabcd;
+    w_write(a3, w3);
+    byte b2 = b_read(a3);
+    byte b3 = b_read(a3+1);
+    word wres3 = ((word)b2) | (b3 << 8); 
+    printf("w-w/2b-r \t %02hhx%02hhx = %04hx; wres3 = %04hx\n", b3, b2, w3, wres3);
+    assert(w3 == wres3);
 }
 
 int main()
@@ -48,16 +66,23 @@ void b_write(Adress adr, byte b)
 }
 byte b_read(Adress adr)
 {
-    return mem[adr];
+    return mem[adr] & 0xFF;
 }
-/* void w_write(Adress adr, word w)
+void w_write(Adress adr, word w)
 {
+    assert(adr % 2 == 0);
+    byte b0, b1;
     
-} */
+    //b0 = (byte)((w << 8) >> 8);
+    b0 = (byte)w;
+    mem[adr] = b0;
+    b1 = (byte)(w >> 8);
+    mem[adr+1] = b1;
+}
 word w_read(Adress adr)
 {
     word w = ((word)mem[adr+1]) << 8;
     //printf("w = %x\n", w);
-    w = w | mem[adr];
+    w = w | (mem[adr] & 0xFF);
     return w;
 }
