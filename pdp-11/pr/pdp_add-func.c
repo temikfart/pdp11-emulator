af10dba28d1cf4761208969dcae394834dd464d4
temikfart <fartygin.a@phystech.edu>
2021-03-28 19:43:03 +0300

    logger as function

---
+++ pdp-11/pr/pdp_add-func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "pdp.h"
+
+int current_log_lvl = INFO;
+
+void logger(int log_lvl, const char * format, ...)
+{
+    if(log_lvl <= current_log_lvl)
+    {
+        va_list ptr;
+        va_start(ptr, format);
+        vprintf(format, ptr);
+        va_end(ptr);
+    }
+}
