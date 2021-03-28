5bb8ea2539de1fd2b5e260be7ed606664b2a3060
temikfart <fartygin.a@phystech.edu>
2021-03-28 20:22:01 +0300

    edited if in logger

--- pdp-11/pr/pdp_add-func.c
+++ pdp-11/pr/pdp_add-func.c
@@ -6,11 +6,10 @@ int current_log_lvl = INFO;
 
 void logger(int log_lvl, const char * format, ...)
 {
-    if(log_lvl <= current_log_lvl)
-    {
-        va_list ptr;
-        va_start(ptr, format);
-        vprintf(format, ptr);
-        va_end(ptr);
-    }
+    if(log_lvl > current_log_lvl)
+        return;
+    va_list ptr;
+    va_start(ptr, format);
+    vprintf(format, ptr);
+    va_end(ptr);
 }
