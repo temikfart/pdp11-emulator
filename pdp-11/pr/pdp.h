1d5d75b5e38fcaee81355b24129d8a0c6388b798
temikfart <fartygin.a@phystech.edu>
2021-03-28 19:56:44 +0300

    new prototypes + comments

--- pdp-11/pr/pdp.h
+++ pdp-11/pr/pdp.h
@@ -1,5 +1,6 @@
 #pragma once
 
+//Constants
 #define MEMSIZE (64*1024)
 
 #define ERROR 0
@@ -7,14 +8,20 @@
 #define TRACE 2
 #define DEBUG 3
 #define SILENT -1
+//====================
 
+//Structures
 typedef unsigned char byte;         //8 bit
 typedef unsigned short int word;    //16 bit
 typedef word Adress;                //16 bit
+//====================
 
+//Global variables
 extern byte mem[MEMSIZE];
 extern int current_log_lvl;
+//====================
 
+//pdp functions
 void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
@@ -22,8 +29,16 @@ word w_read(Adress adr);
 
 void mem_dump(Adress start, word n);
 void load_file();
+//====================
 
+//Additional functions
 void logger(int log_lvl, const char * format, ...);
+//====================
 
+//Tests
+void test_mem_1(word w, byte b1, byte b0);
 void test_mem();
 void test_odd_adr();
+void test_logger_prints();
+void test_logger();
+//====================
