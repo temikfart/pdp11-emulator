b28309b382a0ea72d5f2cbf0575b50b20f8a6b8d
temikfart <fartygin.a@phystech.edu>
2021-03-30 18:06:28 +0300

    new const, structs and funcs

--- pdp-11/pr/pdp.h
+++ pdp-11/pr/pdp.h
@@ -1,14 +1,21 @@
 #pragma once
 
 //Constants
-#define MEMSIZE (64*1024)
+#define MEMSIZE (64*1024)   //Размер памяти PDP-11
+#define pc reg[7]           //Program Counter
 
+//Уровни логирования
 #define FATAL 0
 #define ERROR 1
 #define INFO 2
 #define TRACE 3
 #define DEBUG 4
 #define SILENT -1
+
+//Наличие аргументов в функции
+#define NO_PARAM 0
+#define HAS_DD 1
+#define HAS_SS 2
 //====================
 
 //Structures
@@ -19,13 +26,19 @@ typedef struct
 {
     word adr;
     word val;
-}Arg;                               //Тип данных для SS и DD
+}Arg;                               //Тип данных для dd, ss, ...
+typedef struct
+{
+    Arg dd;
+    Arg ss;
+}Param;                             //Аргументы для функций в одной структуре
 typedef struct
 {
     word mask;
     word opcode;
     char * name;
-    void (* do_func)(Arg dd, Arg ss);
+    char params;
+    void (* do_func)(Param p);
 }Command;                           //Описание для команд PDP-11
 //====================
 
@@ -33,8 +46,7 @@ typedef struct
 extern byte mem[MEMSIZE];       //Память
 extern int current_log_lvl;     //Уровень логирования
 extern word reg[8];             //Регистры R0, ..., R8
-extern Arg dd;
-extern Arg ss;
+extern Param p;                 //Структура для параметров функций cmd[]
 //====================
 
 //PDP main-functions
@@ -46,23 +58,24 @@ word w_read(Adress adr);
 void mem_dump(Adress start, word n);
 void load_file();
 void console_arg(int argc, char * argv[]);
+Arg get_modereg(word w);
 void run();
 //====================
 
 //PDP do-functions
-void do_halt();
-void do_mov();
-void do_add();
-void do_nothing();
-void do_unknown();
+void do_halt(Param p);
+void do_mov(Param p);
+void do_add(Param p);
+void do_nothing(Param p);
+void do_unknown(Param p);
 //====================
 
 //Additional functions
 void logger(int log_lvl, const char * format, ...);
+void reg_print();
 //====================
 
 //Tests
-void test_mem_1(word w, byte b1, byte b0);
 void test_mem();
 void test_odd_adr();
 void test_logger_prints();
