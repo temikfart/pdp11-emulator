a3f6b3f04b89eb6f12a75e78d6d140fe959945c5
temikfart <fartygin.a@phystech.edu>
2021-03-30 18:06:58 +0300

    now funcs with 1 arg

--- pdp-11/pr/pdp_do-func.c
+++ pdp-11/pr/pdp_do-func.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 #include "pdp.h"
 
-void do_halt(Arg dd, Arg ss)
+void do_halt(Param p)
 {
     logger(INFO, "\n-----------------HALT------------------\n");
-    logger(INFO, 
-        "R0=%06o R2=%06o R4=%06o SP=%06o\n", 
-        reg[0], reg[2], reg[4], reg[6]);
-    logger(INFO, 
-        "R1=%06o R3=%06o R5=%06o PC=%06o\n", 
-        reg[1], reg[3], reg[5], reg[7]);
+    reg_print();
+    
     exit(0);
 }
-void do_mov(Arg dd, Arg ss)
+void do_mov(Param p)
 {
-    logger(DEBUG, "\nDebug: mov: R%o = %06o.\n", dd.adr, ss.val);
-    w_write(dd.adr, ss.val);
+    logger(DEBUG, "\nDebug: mov: R%o = %06o.\n", p.dd.adr, p.ss.val);
+    
+    w_write(p.dd.adr, p.ss.val);
 }
-void do_add(Arg dd, Arg ss)
+void do_add(Param p)
 {
-    logger(DEBUG, "\nDebug: add: R%o = R%o + R%o.\n", dd.adr, dd.adr, ss.adr);
-    word w = w_read(dd.adr);
-    w = w + ss.val;
-    w_write(dd.adr, w);
+    logger(DEBUG, "\nDebug: add: R%o = R%o + R%o.\n", p.dd.adr, p.dd.adr, p.ss.adr);
+    
+    word w = w_read(p.dd.adr);
+    w = w + p.ss.val;
+    w_write(p.dd.adr, w);
 }
-void do_nothing(Arg dd, Arg ss)
+void do_nothing(Param p)
 {
     return;
 }
-void do_unknown(Arg dd, Arg ss)
+void do_unknown(Param p)
 {
     logger(ERROR, "unknown\n");
+    
+    exit(1);
 }
