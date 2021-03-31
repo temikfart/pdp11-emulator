7220ac19f169761715d45e0db3b2a9be36ed6d2d
temikfart <fartygin.a@phystech.edu>
2021-03-31 10:42:19 +0300

    get_modereg is shorter than before; SOB in cmd[]; some revisions

--- pdp-11/pr/pdp_run.c
+++ pdp-11/pr/pdp_run.c
@@ -3,12 +3,13 @@
 #include <string.h>
 #include "pdp.h"
 
-//Структура для параметров функций cmd[]
-Param p;
+static Param p;                 //Структура для параметров функций cmd[]
+
 //Массив всех команд
 static Command cmd[] = {
-    {0170000, 0010000, "mov", HAS_DD | HAS_SS, do_mov},
     {0170000, 0060000, "add", HAS_DD | HAS_SS, do_add},
+    {0170000, 0010000, "mov", HAS_DD | HAS_SS, do_mov},
+    {0177000, 0077777, "SOB", HAS_R | HAS_N, do_SOB},
     {0177777, 0000000, "halt", NO_PARAM, do_halt},
     {0000000, 0000000, "unknown", NO_PARAM, do_unknown}
 };
@@ -20,45 +21,20 @@ Arg get_modereg(word w)
     int mode = (w >> 3) & 7;    //Номер моды
     switch(mode)
     {
-        //Mode 0: Rn
-        case 0:
-            res.adr = r;
-            res.val = reg[r];
-            logger(TRACE, "R%o ", r);
+        case 0:             //Mode 0: Rn
+            mode0(&res, r);
             break;
-        //Mode 1: (Rn)
-        case 1:
-            res.adr = reg[r];
-            res.val = w_read(res.adr);  //b_read если операция байтовая
-            logger(TRACE, "(R%o) ", r);
+        case 1:             //Mode 1: (Rn)
+            mode1(&res, r);
             break;
-        //Mode 2: (Rn)+
-        case 2:
-            res.adr = reg[r];
-            res.val = w_read(res.adr);  //b_read если операция байтовая
-            reg[r] += 2;                //+= 1 если операция байтовая
-            if(r == 7)
-                logger(TRACE, "#%o ", res.val);
-            else
-                logger(TRACE, "(R%o)+ ", r);
+        case 2:             //Mode 2: (Rn)+
+            mode2(&res, r);
             break;
-        //Mode 4: -(Rn)
-        case 4:
-            if(r == 7 && pc == 0)
-            {
-                logger(FATAL, "Fatal error: Array index out of bounds.\n");
-                exit(1);
-            }
-            reg[r] -= 2;                //-= 1 если операция байтовая
-            res.adr = reg[r];
-            res.val = w_read(res.adr);  //b_read если операция байтовая
-            if(r == 7)
-                logger(TRACE, "#%o ", res.val);
-            else
-                logger(TRACE, "(R%o)+ ", r);
+        case 4:             //Mode 4: -(Rn)
+            mode4(&res, r);
             break;
         default:
-            logger(ERROR, "Mode %o not implemented yet.\n", r);
+            logger(ERROR, "Mode %o not implemented yet.\n", mode);
             exit(1);
     }
     
@@ -70,23 +46,11 @@ Param get_params(word w, char params)
     
     Param res;
     
-    if(params == NO_PARAM)
-    {
-        //logger(DEBUG, "No params\n");
-        
-        return res;
-    }
-    
     if((params & HAS_SS) == HAS_SS)
-    {
-        //logger(DEBUG, "\nhas a SS-parameter\n");
         res.ss = get_modereg(w >> 6);
-    }
+    
     if((params & HAS_DD) == HAS_DD)
-    {
-        //logger(DEBUG, "\nhas a DD-parameter\n");
         res.dd = get_modereg(w);
-    }
     
     return res;
 }
