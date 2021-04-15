4a0e2aac8adb2e1b8c7d72aad8ac67bc693f43db
raman <hankouraman@gmail.com>
2021-04-15 20:27:54 +0300

    feat(23): in the do_.. functions were added the setting of psw flags

--- pdp-11/pr/src/pdp_do_func.c
+++ pdp-11/pr/src/pdp_do_func.c
@@ -12,41 +12,57 @@ void do_halt(Param p) {
   result_print(); 
   exit(0);
 }
+
 void do_mov(Param p) {
-  logger(DEBUG,
-         "\nR%o = %06o.\n", p.dd.adr, p.ss.val);
-  
+  logger(DEBUG, "\nR%o = %06o.\n", p.dd.adr, p.ss.val);
+
   if (p.is_byte_cmd) {
     b_write(p.dd.adr, p.ss.val);
   } else {
     w_write(p.dd.adr, p.ss.val);
   }
+
+  // NZVC == **0-
+  set_NZ(p.ss.val, p.is_byte_cmd);
 }
+
 void do_add(Param p) {
-  logger(DEBUG,
-         "\nR%o = R%o + R%o.\n",
-         p.dd.adr, p.dd.adr, p.ss.adr);
+  logger(DEBUG, "\nR%o = R%o + R%o.\n", p.dd.adr, p.dd.adr, p.ss.adr);
   
-  word w = w_read(p.dd.adr);
+  uint32_t w = w_read(p.dd.adr);
   w = w + p.ss.val;
   w_write(p.dd.adr, w);
+
+  // NZVC == ****
+  set_NZ(w, p.is_byte_cmd);
+  set_C(w, p.is_byte_cmd);
 }
+
 void do_sob(Param p) {
   reg[p.r]--;
   if (reg[p.r] != 0) {
     pc = pc - 2*p.nn;
   }
-  
+
+  // NZVC == ----
+
   logger(DEBUG, "\nR%o = %o, PC = %o.\n", p.r, reg[p.r], pc);
 }
+
 void do_clr(Param p) {
   logger(DEBUG, "\nmem[%o] = 0.\n", p.dd.adr);
   
   w_write(p.dd.adr, 0);
+
+  // NZVC == 0100
+  set_NZ(0, p.is_byte_cmd);
+  set_C(0, p.is_byte_cmd);
 }
+
 void do_nothing(Param p) {
   return;
 }
+
 void do_unknown(Param p) {
   logger(ERROR, "unknown\n");
   
@@ -60,6 +76,7 @@ void do_tst(Param p) {
   // p.dd.val -- it is the value for which we set flags N, Z flags
   word tested_value = p.dd.val;
   
+  // NZVC == **00
 	set_NZ(tested_value, p.is_byte_cmd);
 	set_C(0, p.is_byte_cmd);
 
@@ -73,6 +90,8 @@ void do_cmp(Param p) {
 
   uint32_t tested_value = left_value - right_value;
   
+
+  // NZVC == ****
   set_NZ(tested_value, p.is_byte_cmd);
   set_C(tested_value, p.is_byte_cmd);
 
